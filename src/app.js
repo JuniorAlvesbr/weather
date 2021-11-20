fce04ed6e9b0d3009e652657fef8f439162bd8b3
Junior Alves <jrnalves@gmail.com>
2021-11-20 03:28:08 -0300

    finish js script

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
+const $main = document.querySelector('.main')
 const $form = document.querySelector('.form')
+const $wrapper = document.querySelector('.wrapper')
 const $cityName = document.querySelector('.cityName')
 const $icon = document.querySelector('.main-weather > .icon')
 const $weather = document.querySelector('.weather')
-const $temperature = document.querySelector('[data-js="temperature"]')
+const $mainTemperature = document.querySelector('[data-js="main-temperature"]')
 const $pressure = document.querySelector('[data-js="pressure"]')
 const $relativeHumidity = document.querySelector('[data-js="relativeHumidity"]')
 const $windSpeed = document.querySelector('[data-js="windSpeed"]')
+const $weekDay = document.querySelectorAll('[data-js="weekDay"]')
+const $fiveIcon = document.querySelectorAll('[data-js="fiveIcon"]')
+const $temperature = document.querySelectorAll('[data-js="temperature"]')
 
+console.log($main)
 
 $form.addEventListener('submit', async event => {
     event.preventDefault()
 
     const inputValue = event.target.inputCity.value
+    const dataInfoApi = await getInfoAPI(inputValue)
 
-    const data = await getWeather(inputValue)
-
-    console.log(data)
-    showDataInHTML(data)
+    showDataInHTML(dataInfoApi)
 })
 
 const showDataInHTML = ({
@@ -27,13 +31,27 @@ const showDataInHTML = ({
     Temperature,
     RelativeHumidity,
     Wind,
-    Pressure
+    Pressure,
+    dataForecastFiveDays
 }) => {
     $cityName.textContent = LocalizedName
     $icon.src = `assets/icons/${WeatherIcon}.svg`
     $weather.textContent = WeatherText
-    $temperature.textContent = Temperature.Metric.Value
+    $mainTemperature.textContent = Temperature.Metric.Value
     $pressure.textContent = Pressure.Metric.Value
     $relativeHumidity.textContent = RelativeHumidity
     $windSpeed.textContent = Wind.Speed.Metric.Value
+    showFiveDaysinHTML(dataForecastFiveDays)
+
+    IsDayTime ? $main.classList.remove('-dark') : $main.classList.add('-dark')
+    $form.reset()
+    $wrapper.classList.add('-active')
+}
+
+const showFiveDaysinHTML = (data) => {
+    data.forEach(({ weekDay, icon, temperature }, index) => {
+        $weekDay[index].textContent = weekDay
+        $fiveIcon[index].src = `assets/icons/${icon}.svg`
+        $temperature[index].textContent = temperature
+    })
 }
--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,5 @@
-const APIKEY = '5zq3p2iSOCT2q8SAWqu98PgeqNddVjGY'
+const APIKEY = 'wjkUMrGTkT7jLWLXedKz0ePKUG272abz'
+const daysOFtheWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 
 const getCityURl = inputValue =>
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKEY}&q=${inputValue}`
@@ -6,19 +7,67 @@ const getCityURl = inputValue =>
 const getWeatherURL = Key =>
     `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKEY}&language=pt-br&details=true`
 
+const getForecastFiveDaysURL = Key =>
+    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${Key}?apikey=${APIKEY}&metric=true`
+
+
+const getInfoAPI = async (inputValue) => {
+    const [{ LocalizedName, Key }] = await getCity(inputValue)
+
+    const dataWeather = await getWeather(Key)
+    const dataForecastFiveDays = await getForecastFiveDays(Key)
+
+    dataWeather['LocalizedName'] = LocalizedName
+    dataWeather['dataForecastFiveDays'] = dataForecastFiveDays
+
+    return dataWeather
+}
+
 const getCity = async (inputValue) => {
     const url = getCityURl(inputValue)
 
     return await (await fetch(url)).json()
 }
 
-const getWeather = async inputValue => {
-    const [{ LocalizedName, Key }] = await getCity(inputValue)
-    const url = getWeatherURL(Key)
+const getWeather = async Key => {
+    const weatherURL = getWeatherURL(Key)
 
-    const [{ WeatherText, WeatherIcon, IsDayTime, Temperature, RelativeHumidity, Wind, Pressure }] = await (await fetch(url)).json()
+    const [{
+        WeatherText,
+        WeatherIcon,
+        IsDayTime,
+        Temperature,
+        RelativeHumidity,
+        Wind,
+        Pressure
+    }] = await (await fetch(weatherURL)).json()
 
     return {
-        LocalizedName, WeatherText, WeatherIcon, IsDayTime, Temperature, RelativeHumidity, Wind, Pressure
+        WeatherText,
+        WeatherIcon,
+        IsDayTime,
+        Temperature,
+        RelativeHumidity,
+        Wind,
+        Pressure
     }
+}
+
+const getForecastFiveDays = async Key => {
+    const forecastFiveDays = []
+    const forecastFiveDaysURL = getForecastFiveDaysURL(Key)
+    const { DailyForecasts } = await (await fetch(forecastFiveDaysURL)).json()
+
+    DailyForecasts.map(item => {
+        const getWeekDay = new Date(item.Date).getDay()
+        const weekDay = daysOFtheWeek[getWeekDay]
+
+        forecastFiveDays.push({
+            'weekDay': weekDay,
+            'icon': item.Day.Icon,
+            'temperature': Math.round(item.Temperature.Maximum.Value)
+        })
+    })
+
+    return forecastFiveDays
 }
